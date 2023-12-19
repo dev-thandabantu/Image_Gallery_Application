7c4a6c131e34bca13eb9466a879601853d2220a6
Brighton Tandabantu <brighton@kindredteam.com>
2023-12-19 17:41:19 +0200

    Add project files.

---
+++ Lucra-Assessment_Image_Gallery/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Lucra_Assessment_Image_Gallery.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lucra_Assessment_Image_Gallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lucra-Assessment_Image_Gallery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lucra_Assessment_Image_Gallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

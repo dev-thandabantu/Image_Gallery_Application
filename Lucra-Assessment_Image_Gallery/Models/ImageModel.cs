d7dfafe5e7fcc90f5265f705c68b0ffe63f80e13
Brighton Tandabantu <brighton@kindredteam.com>
2023-12-20 18:43:42 +0200

    Create a model for the image, install EF Core packages, and add database connection string.

---
+++ Lucra-Assessment_Image_Gallery/Models/ImageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Lucra_Assessment_Image_Gallery.Models
+{
+	public class ImageModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
